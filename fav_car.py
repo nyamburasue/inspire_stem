b45b14dd235bda3152036930974a008fa2eaedc0
nyamburasue <159990154+nyamburasue@users.noreply.github.com>
2024-02-28 20:04:26 +0300

    Add files via upload

---
+++ fav_car.py
@@ -0,0 +1,14 @@
+#program to show the description of my favourite car
+#name : Susan Maina
+#date :28/02/2024
+
+fav_car ={"make" :"bugatti","color":"black",
+        "year":"2010"}
+
+print(fav_car)
+
+print(fav_car["make"])
+print(fav_car["color"])
+print(fav_car["year"])
+
+fav_car.copy()
