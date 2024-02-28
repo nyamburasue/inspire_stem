04d54f6ca776be86356b51ea00276f0178d39cce
nyamburasue <159990154+nyamburasue@users.noreply.github.com>
2024-02-28 20:06:07 +0300

    Add files via upload

---
+++ hobbies.py
@@ -0,0 +1,13 @@
+#program to show a tuple of hobbies
+#name : Susan Maina
+#date :28/02/2024
+
+hobbies = ("swimming","travelling","drawing","cooking")
+print(hobbies)
+
+fav_car ={"make" :"bugatti","colour":"black","length" :"35 inches",
+        "year":"2010"}
+
+print(fav_car["make"])
+print(fav_car["colour "])
+print(fav_car["year"])
